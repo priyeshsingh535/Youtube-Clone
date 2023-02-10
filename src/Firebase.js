b36466ece3a6907facff9deba34d279e1d38287f
priyesh kumar <59879071+priyeshsingh535@users.noreply.github.com>
2023-02-10 09:55:01 +0530

    First commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDi4_mvMNiCUEj2d8cAT0jITEGzJnNM9r4",
-    authDomain: "clone-81594.firebaseapp.com",
-    projectId: "clone-81594",
-    storageBucket: "clone-81594.appspot.com",
-    messagingSenderId: "974102418137",
-    appId: "1:974102418137:web:82bce8a1ba09807770a4c8",
-    measurementId: "G-6L64LGKTFQ"
-  };
-
-  //Generate this from firebase.com
+  apiKey: "AIzaSyAPQ5vB2yh8hhgxAN0w6IlD3suIxaXIV5M",
+  authDomain: "clone-5cfba.firebaseapp.com",
+  projectId: "clone-5cfba",
+  storageBucket: "clone-5cfba.appspot.com",
+  messagingSenderId: "688268425598",
+  appId: "1:688268425598:web:483556c6a62bd6354a1b2f",
+  measurementId: "G-QHC805YMKV"
+};
